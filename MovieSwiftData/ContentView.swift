c15e727086aa9be59f1c9b7029fe5e1d731a37a4
sangam <sangampokharel803@gmail.com>
2025-04-30 14:10:34 +0545

    Initial Commit

---
+++ MovieSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieSwiftData
+//
+//  Created by sangam pokharel on 30/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieSwiftData/MovieSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSwiftDataApp.swift
+//  MovieSwiftData
+//
+//  Created by sangam pokharel on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
